42aa229bb716791488412f6005dd2a05d66e3007
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 00:44:34 +0100

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="dashboard.php">Home</a></li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="orders.php">Orders</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+		</ul>
+	</nav>
+	
+	<h1>Welcome to the Dashboard!</h1>
+	<p>Here you can manage your products and orders.</p>
+	
+	<!-- Your code for the rest of the dashboard page goes here -->
+	
+</body>
+</html>
