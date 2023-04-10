e234a46af26a9d26a9dbb3f83e5cf490e35076fe
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 00:49:54 +0100

    Create admin_dashboard.php

---
+++ admin_dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Dashboard</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="admin_dashboard.php">Home</a></li>
+			<li><a href="admin_products.php">Products</a></li>
+			<li><a href="admin_orders.php">Manage Orders</a></li>
+			<li><a href="admin_users.php">Manage Users</a></li>
+			<li><a href="logout.php">Logout</a></li>
+		</ul>
+	</nav>
+	
+	<h1>Welcome to the Admin Dashboard!</h1>
+	<p>Here you can manage products, orders, and users.</p>
+	
+	<!-- Your code for the rest of the admin dashboard page goes here -->
+	
+</body>
+</html>
