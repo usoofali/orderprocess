3c7bfac349a029e182d8e50abd57548a0a5bb91f
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 23:20:11 +0100

    Update order.sql

--- order.sql
+++ order.sql
@@ -35,7 +35,7 @@ CREATE TABLE order_items (
   order_id INT(11) NOT NULL,
   product_id INT(11) NOT NULL,
   quantity INT(11) NOT NULL,
-  price INT(11) NOT NULL,
+  total_price INT(11) NOT NULL,
   FOREIGN KEY (order_id) REFERENCES orders(id),
   FOREIGN KEY (product_id) REFERENCES product(id)
 );
