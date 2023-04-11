5e5f9a3c948f5b2c829ba364c7ce4ef8d26708e1
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 01:30:51 +0100

    Update product.php

--- product.php
+++ product.php
@@ -1,76 +1,80 @@
-<?php
-// Start session
-session_start();
 
+
+<?php
+// Connect to the database
+// Start session
 // Include database connection file
 include('dbconnect.php');
-
-// Query to select all products from the products table
-$sql = "SELECT * FROM products";
-
-// Execute the query and store the result set
-$result = mysqli_query($conn, $sql);
-
-// Check if there are any products
-if (mysqli_num_rows($result) > 0) {
-	// Products are available, display them in a scrollable list
-	echo '<h1>Products</h1>';
-	echo '<div style="max-height: 400px; overflow-y: scroll;">';
-	echo '<table>';
-	echo '<thead><tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Quantity</th><th>Action</th></tr></thead>';
-	echo '<tbody>';
-	while ($row = mysqli_fetch_assoc($result)) {
-		// Display each product in a row of the table
-		echo '<tr>';
-		echo '<td>' . $row['id'] . '</td>';
-		echo '<td>' . $row['name'] . '</td>';
-		echo '<td>' . $row['description'] . '</td>';
-		echo '<td>' . $row['price'] . '</td>';
-		echo '<td>';
-		echo '<select name="quantity">';
-		for ($i = 1; $i <= $row['quantity']; $i++) {
-			echo '<option value="' . $i . '">' . $i . '</option>';
-		}
-		echo '</select>';
-		echo '</td>';
-		echo '<td><button onclick="addToCart(' . $row['id'] . ')">Add to Cart</button></td>';
-		echo '</tr>';
-	}
-	echo '</tbody>';
-	echo '</table>';
-	echo '</div>';
-} else {
-	// No products available
-	echo '<p>No products available.</p>';
+session_start();
+if (!isset($_SESSION['user_id'])) {
+	header('Location: login.php');
+	exit();
 }
 
-// Close database connection
-mysqli_close($conn);
+// Handle adding or removing items from the cart
+if (isset($_POST['add_to_cart'])) {
+	// Get the quantity and product ID from the form
+	$quantity = $_POST['quantity'];
+	$product_id = $_POST['product_id'];
+	
+	// Insert the order into the orders table
+	$user_id = $_SESSION['user_id'];
+	$sql = "INSERT INTO orders (user_id, status) VALUES ('$user_id', 'incomplete')";
+	if ($conn->query($sql) === TRUE) {
+		$order_id = $conn->insert_id;
+		
+		// Insert the item into the order_items table
+		$sql = "INSERT INTO order_items (order_id, product_id, quantity) VALUES ('$order_id', '$product_id', '$quantity')";
+		if ($conn->query($sql) === TRUE) {
+			echo "Item added to cart successfully.";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+} elseif (isset($_POST['remove_from_cart'])) {
+	// Get the order item ID from the form
+	$order_item_id = $_POST['order_item_id'];
+	
+	// Delete the order item from the order_items table
+	$sql = "DELETE FROM order_items WHERE id='$order_item_id'";
+	if ($conn->query($sql) === TRUE) {
+		echo "Item removed from cart successfully.";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+
+// Select all items from the product table
+$sql = "SELECT * FROM product";
+$result = $conn->query($sql);
 ?>
 
-<script>
-// Function to add item to cart
-function addToCart(productId) {
-	// Send AJAX request to add item to cart
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'add_to_cart.php', true);
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhr.onload = function() {
-		if (this.status == 200) {
-			alert('Item added to cart.');
-		}
-	};
-	xhr.send('productId=' + productId + '&quantity=' + document.getElementsByName('quantity')[productId - 1].value);
-}
-</script>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Products</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="dashboard.php">Home</a></li>
+			<li><a href="product.php">Products</a></li>
+			<li><a href="orders.php">Orders</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+		</ul>
+	</nav>
+	
+	<h1>Products</h1>
+	
+	<?php
+	if ($result->num_rows > 0) {
+		// Display each product in a scrollable list
+		echo '<div style="overflow-y: scroll; height: 500px;">';
+		while ($row = $result->fetch_assoc()) {
+			echo '<div style="border: 1px solid black; padding: 10px; margin-bottom: 10px;">';
+			echo '<h3>' . $row['name'] . '</h3>';
+			echo '<p>Price: ' . $row['price']
 
-<!-- Navigation bar for dashboard.php -->
-<nav>
-	<ul>
-		<li><a href="dashboard.php">Home</a></li>
-		<li><a href="product.php">Products</a></li>
-		<li><a href="orders.php">Orders</a></li>
-		<li><a href="about.php">About</a></li>
-		<li><a href="contact.php">Contact Us</a></li>
-	</ul>
-</nav>
