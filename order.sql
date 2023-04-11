99bf87a81493737c560afdeb679454feb8fd4fa1
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 22:51:52 +0100

    Update order.sql

--- order.sql
+++ order.sql
@@ -22,6 +22,7 @@ CREATE TABLE product (
 CREATE TABLE orders (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   user_id INT(11) NOT NULL,
+  status varchar(15) NOT NULL DEFAULT 'Pending',
   completed TINYINT(1) NOT NULL DEFAULT '0',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -33,6 +34,7 @@ CREATE TABLE order_items (
   order_id INT(11) NOT NULL,
   product_id INT(11) NOT NULL,
   quantity INT(11) NOT NULL,
+  price INT(11) NOT NULL,
   FOREIGN KEY (order_id) REFERENCES orders(id),
   FOREIGN KEY (product_id) REFERENCES product(id)
 );
