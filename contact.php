2993600427a4956e8b93b7ebe32888a008796d72
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 03:59:09 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Us</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="dashboard.php">Home</a></li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="orders.php">Orders</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+      <li><a href="logout.php">Logout</a></li>
+		</ul>
+	</nav>
+	
+	<h1>Contact Us</h1>
+	
+	<p>Thank you for your interest in Hamstores Integrated Services!</p>
+	
+	<h2>Our Address</h2>
+	<p>
+		123 Main Street<br>
+		Gusau, Zamfara State<br>
+		Nigeria
+	</p>
+	
+	<h2>Phone Number</h2>
+	<p>(123) 456-7890</p>
+	
+	<h2>Email Address</h2>
+	<p>info@hamstores.com</p>
+	
+	<!-- Your code for the rest of the contact page goes here -->
+	
+</body>
+</html>
