e5fd95292065192aee9b7569142b3be31fd4420c
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 03:19:28 +0100

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Payment Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="navbar">
+		<a href="#">Home</a>
+		<a href="#">Product</a>
+		<a class="active" href="#">Payment</a>
+		<a href="#">Orders</a>
+		<a href="#">About</a>
+		<a href="#">Contact Us</a>
+	</div>
+
+	<div class="container">
+		<h1>Payment</h1>
+		<?php
+			// Connect to the database
+			$servername = "localhost";
+			$username = "username";
+			$password = "password";
+			$dbname = "order";
+
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
+			if (!$conn) {
+			    die("Connection failed: " . mysqli_connect_error());
+			}
+
+			// Insert the order information into the orders table
+			$user_id = 1; // replace with the actual user id
+			$sql = "INSERT INTO orders (user_id, status) VALUES ($user_id, 'paid')";
+			if (mysqli_query($conn, $sql)) {
+			    $order_id = mysqli_insert_id($conn);
+
+			    // Insert the order items into the order_items table
+			    foreach ($_POST['selected_product'] as $product_id) {
+			    	$quantity = $_POST['quantity'][$product_id];
+			    	$sql = "INSERT INTO order_items (order_id, product_id, quantity) VALUES ($order_id, $product_id, $quantity)";
+			    	mysqli_query($conn, $sql);
+			    }
+
+			    // Update the order status to "complete"
+			    $sql = "UPDATE orders SET status='complete' WHERE id=$order_id";
+			    mysqli_query($conn, $sql);
+
+			    // Display a message to the user confirming the purchase
+			    echo '<p>Thank you for your purchase!</p>';
+			} else {
+			    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+
+			// Close the database connection
+			mysqli_close($conn);
+		?>
+	</div>
+</body>
+</html>
