5c4a2c2586b39f1efd9d1bc127d46c7d75629525
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-04-11 03:56:33 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Hamstores Integrated Services</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="dashboard.php">Home</a></li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="orders.php">Orders</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+		</ul>
+	</nav>
+	
+	<h1>About Hamstores Integrated Services</h1>
+	<p>Hamstores Integrated Services is a leading provider of e-commerce solutions in Gusau, Zamfara State. We specialize in creating custom online stores for businesses of all sizes, from small startups to large corporations.</p>
+	
+	<p>Our team of experienced developers and designers is dedicated to providing high-quality, reliable services that help our clients grow and succeed. We work closely with our clients to understand their unique needs and goals, and we tailor our solutions to meet those needs.</p>
+	
+	<p>Whether you're looking to start a new online store, improve an existing one, or simply learn more about e-commerce, we're here to help. Contact us today to learn more about our services and how we can help your business succeed online.</p>
+</body>
+</html>
